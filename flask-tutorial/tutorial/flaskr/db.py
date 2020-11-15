ffaa88c0900436d5380aba286ba73e37e9bbdd1d
onion <kosukekito38@gmail.conm>
2020-11-15 11:59:04 +0900

    flask-tutorial / finished Blueprints and Views

--- flask-tutorial/tutorial/flaskr/db.py
+++ flask-tutorial/tutorial/flaskr/db.py
@@ -21,7 +21,7 @@ def close_db(e=None):
 def init_db():
     db = get_db()
     with current_app.open_resource('./schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+        db.executespcript(f.read().decode('utf8'))
 
 @click.command('init-db')
 @with_appcontext
