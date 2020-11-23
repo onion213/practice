d9f62351fe5125ddba96e03079904b0ea148dfcc
onion <kosukekito38@gmail.conm>
2020-11-23 20:43:13 +0900

    flask-todo / add test of db connection to helloworld.

---
+++ flask-todo/flask/hello/db.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE hello;
+
+CREATE TABLE people (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(16) NOT NULL
+);
--- flask-todo/flask/hello/hello.py
+++ flask-todo/flask/hello/hello.py
@@ -1,10 +1,34 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    con = db_con()
+
     return 'Hello, World!'
+
+@app.route('/<string:name>/')
+def hello_with_name(name):
+    con = db_con()
+    cursor = con.cursor()
+    cursor.execute('SELECT name FROM people')
+    names =  cursor.fetchall()
+    if (name,) in names:
+        return 'Hello, {}!'.format(name)
+    else:
+        return 'Not Found'
+
+def db_con():
+    config = {
+        'user':'docker', 
+        'host':'db', 
+        'password': 'docker', 
+        'database':'hello'
+    }
+    con = mysql.connector.connect(**config)
+    return con;
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
