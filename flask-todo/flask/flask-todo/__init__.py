3fe4f986ba728a95d4d67ba0617e303cee6c575c
onion <kosukekito38@gmail.conm>
2020-11-29 18:51:11 +0900

    flask-todo / making home.

--- flask-todo/flask/flask-todo/__init__.py
+++ flask-todo/flask/flask-todo/__init__.py
@@ -1,21 +1,60 @@
-from flask import Flask, render_template, g, request, redirect, url_for
+from flask import Flask, render_template, g, request, redirect, url_for, session, flash
+from flask-todo import db
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 def create_app():
     app = Flask(__name__)
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
-        if request.method == 'GET':
-            try:
-                g.user
-                return redirect(url_for('/home'))
-            except AttributeError:
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            db = db.connect_db()
+            cursor = db.cursor()
+            user = cursor.execute(
+                'SELECT * FROM users WHERE username = %s', (username,)
+            ).fetchone()
+            error = None
+
+            if user is None:
+                error = 'Invalid username.'
+            if check_password_hash(user['password'], password):
+                error = 'Invalid password.'
+            
+            if error is None:
+                session.clear()
+                session['uid'] = user['id']
+                return redirect(url_for('home'))
+            
+            flash(error)
+
+            return render_template('auth/login.html')
+
+        else:
+            uid = session.get('uid')
+            if uid is None:
+                return redirect(url_for('home'))
+            else:
                 return render_template('auth/login.html')
-        return "a"
+        
     
     @app.route('/register')
     def register():
         return render_template('auth/register.html')
+
+    @app.route('/home')
+    def home():
+        uid = session.get('uid')
+        if uid is None:
+            return redirect(url_for('login'))
+        db = db.connect_db()
+        cursor = db.cursor
+        todos = cursor.execute(
+            'SELECT * FROM todos WHERE user_id = %s', (uid,)
+        ).fetchall()
+        return render_template('main/home.html', todos=todos)
 
     return app
 
