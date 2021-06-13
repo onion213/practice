4874efb3ca52430b1895880cb0675dfc0885faa1
onion <kosukekito38@gmail.com>
2021-06-13 22:50:49 +0900

    play with jupytext

---
+++ jupytext-practice/jupytext-test.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:hydrogen
+#     text_representation:
+#       extension: .py
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.11.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Markdown Block
+
+# %% [raw]
+# Text Block
+
+# %%
+for i in range(1000):
+    print(i)
+
+# %%
